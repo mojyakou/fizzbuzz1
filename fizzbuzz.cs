c88af5141c3cd883ba73db9fd9098b454739c8f7
mojyakou <justtom.0316.yunoya.rino1@gmail.com>
2015-03-10 09:27:17 +0900

    もう少しLINQっぽくしてみました

--- fizzbuzz.cs
+++ fizzbuzz.cs
@@ -1,32 +1,27 @@
 void Main()
 {
 	// 100までFizzBuzz
-	FizzBuzzGenerate().Take(100).Dump();
+	Enumerable.Range(1, 100)
+		.Select(x=> FuzzBizz(x))
+		.Dump();
 }
 
-// FizzBuzz作成
-static IEnumerable<string> FizzBuzzGenerate()
+static string FuzzBizz(int i)
 {
-   int i = 1;
-   // iのoverflowはあえて無視
-   while (true)
-   {
-       if (i % 15 == 0)
-       {
-           yield return "FizzBuzz";
-       }
-       else if (i % 3 == 0)
-       {
-            yield return "Fizz";
-       }
-       else if (i % 5 == 0)
-       {
-            yield return "Buzz";
-       }
-       else
-       {
-            yield return i.ToString();
-       }
-	   i++;
-   }
+	if (i % 15 == 0)
+	{
+		return "FizzBuzz";
+	}
+	else if (i % 3 == 0)
+	{
+		return "Fizz";
+	}
+	else if (i % 5 == 0)
+	{
+		return "Buzz";
+	}
+	else
+	{
+		return i.ToString();
+	}
 }
